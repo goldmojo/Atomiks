7d9f30d8ef1c27fb63efad54a161e57a3919b84e
goldmojo <goldmojo@free.fr>
2020-09-05 13:40:26 +0200

    Set RG350 key mapping

--- drv_inp.c
+++ drv_inp.c
@@ -24,22 +24,39 @@ static enum atomiks_keys sdlkey2atomiks(int sdlkey) {
     case SDLK_DOWN:
     case SDLK_KP_2:
       return(atomiks_down);
+    #ifdef __GCW0__
+    case SDLK_LCTRL: /*A*/
+    case SDLK_LALT: /*B*/
+    #else
     case SDLK_RETURN:
     case SDLK_KP_5:
     case SDLK_KP_ENTER:
       if (SDL_GetModState() & KMOD_ALT) return(atomiks_fullscreen);
+    #endif
       return(atomiks_enter);
+    #ifdef __GCW0__
+    case SDLK_LSHIFT: /*Y*/
+    case SDLK_TAB: /*L1*/
+    #else
     case SDLK_HOME:
     case SDLK_KP_7:
+    #endif
       return(atomiks_home);
+    #ifdef __GCW0__
+    case SDLK_SPACE: /*X*/
+    case SDLK_BACKSPACE: /*R1*/
+    #else
     case SDLK_END:
     case SDLK_KP_1:
+    #endif
       return(atomiks_end);
-    case SDLK_ESCAPE:
+    case SDLK_ESCAPE: /*SELECT*/
       return(atomiks_esc);
+    #ifndef __GCW0__
     case SDLK_LALT: /* ALT presses shall be ignored */
     case SDLK_RALT:
       return(atomiks_none);
+    #endif
     default:
       return(atomiks_unknown);
   }
